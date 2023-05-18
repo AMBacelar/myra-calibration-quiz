83af38d53e2edab65a0885d9e828ffa5d8963af5
AMBacelar <a.m.bacelar@gmail.com>
2023-05-18 14:28:08 +0100

    I wish this library was typed...

--- src/lib/brevo/send-waitlist-email.ts
+++ src/lib/brevo/send-waitlist-email.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import SibApiV3Sdk from "sib-api-v3-sdk";
@@ -5,13 +6,12 @@ import SibApiV3Sdk from "sib-api-v3-sdk";
 SibApiV3Sdk.ApiClient.instance.authentications["api-key"].apiKey =
   process.env.BREVO_API_KEY;
 
-export const sendWaitlistEmail = (email: string, name: string) => 
-  new SibApiV3Sdk.TransactionalEmailsApi()
-    .sendTransacEmail({
-      templateId: 11,
-      params: {
-        greeting: "This is the default greeting",
-        headline: "This is the default headline",
-      },
-      to: [{ name, email }],
-    })
+export const sendWaitlistEmail = (email: string, name: string) =>
+  new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail({
+    templateId: 11,
+    params: {
+      greeting: "This is the default greeting",
+      headline: "This is the default headline",
+    },
+    to: [{ name, email }],
+  });
